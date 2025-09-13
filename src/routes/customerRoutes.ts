8a17cffe949fbf728b3208b233b24b7f523d9c90
Gerardo Gastelum <ggastelum@viajeslili.com>
2025-09-13 10:23:24 -0600

    [ADD] to pruebas

--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -4,10 +4,5 @@ import { CustomerController } from '../controllers/customerController';
 const router = Router();
 
 // Customer routes
-router.get('/', CustomerController.getAllCustomers);
-router.get('/:id',CustomerController.getCustomersById);
-router.post('/', CustomerController.createCustomer);
-router.put('/:id',CustomerController.updateCustomer);
-router.delete('/:id',CustomerController.deleteCustomer);
 
 export default router;
